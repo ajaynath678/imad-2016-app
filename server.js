aaaebf539e873429811c0471e658233bb406de4a
ajaynath678 <ajaynath678@gmail.com>
2016-09-19 18:51:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one' : {
+        title : "Article One | Ajay Nath",
+        heading : "Article One",
+        date : "Sept 5, 2016",
+        content : ` <p>
+                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
+                </p>
+                
+                 <p>
+                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
+                </p>
+                
+                 <p>
+                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
+                </p>` 
+    },
+    'article-two' : {
+        title : "Article Two | Ajay Nath",
+        heading : "Article Two",
+        date : "Sept 10, 2016",
+        content : ` <p>
+                    This is the content of my second article. 
+                </p>`
+    },
+    'article-three' : {
+        title : "Article Three | Ajay Nath",
+        heading : "Article Three",
+        date : "Sept 19, 2016",
+        content : ` <p>
+                    This is the content of my third article.
+                </p>`
+    }
+};
+
+function createTemplate(data) {
+    
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+        <html>
+        <head>
+            <title>${title}</title>
+            <meta name="viwport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                
+                <div>
+                   ${content}
+                </div>
+            </div>
+        </body>
+        
+        </html>`;
+
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function(req, res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function(req, res) {
