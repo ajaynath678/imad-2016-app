a7d0cc10be73d6797aab18d249f1aca5e9d03c2b
ajaynath678 <ajaynath678@gmail.com>
2016-09-29 01:44:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+  
+  alert("Hiiii");
+  
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
